b0641197fffc32100a7864dce1c73e85e9ca223b
nikhil-asudani <nikhilasudani01@gmail.com>
2026-04-18 21:06:08 +0530

    Fix Y-axis to constant 0-10 scale

--- app/page.tsx
+++ app/page.tsx
@@ -381,7 +381,8 @@ function DailyLoadChart({ analyst }: { analyst: Analyst }) {
               axisLine={false}
               tickFormatter={(value) => `${value} pts`}
               width={45}
-              domain={[0, 'auto']}
+              domain={[0, 10]}
+              ticks={[0, 2, 4, 6, 8, 10]}
             />
             <Tooltip content={<ChartTooltip />} cursor={false} />
             <ReferenceLine
